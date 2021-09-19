ae39f28d081e1632dadf93fa132963a228d19a57
amirhosein farhoodi <farhoodiamirhosein@gmail.com>
2021-09-19 09:16:26 -0800

    fixbugs

--- src/ModalFlow.tsx
+++ src/ModalFlow.tsx
@@ -12,26 +12,51 @@ export const ModalFlow = ({
   closeOnOutsideClick = false,
   components = {},
   onClose,
-  activeKey
-}: {
-  [key: string]: any;
-}) => {
-  const [activeKeyLocal, setActiveKeyLocal] = React.useState(activeKey);
-  const [values, setValues] = React.useState({});
+  activeKey,
+  onActiveKeyChange,
+  initialValues,
+  bodyStyles,
+  modalStyles
+}: { [key: string]: any; }) => {
+
+  const [prevKey , setPrevKey] = React.useState('');
+  const [activeKeyLocal, setActiveKeyLocal] = React.useState('');
+  const [values, setValues] = React.useState(initialValues ? {...initialValues} : {});
   const [childrens, setChildrens] = React.useState(null);
   const componentRef = React.useRef();
 
-  function handleClickOutside(event: any) {
+  console.log({activeKeyLocal,activeKey})
+
+  const handleActiveKeyChange = (value) => {
+    setPrevKey(activeKeyLocal);
+    if(!onActiveKeyChange){
+      setActiveKeyLocal(value);
+    }else{
+      onActiveKeyChange(value);
+    }
+  }
+
+  const close = () => {
+    if(onClose){
+      onClose();
+    }else{
+      setActiveKeyLocal('');
+    }
+  };
+
+  React.useEffect(() => {
+    setActiveKeyLocal(activeKey);
+  },[activeKey])
+
+  const handleClickOutside = React.useCallback((event: any) => {
     if (
       event &&
       event.target &&
       componentRef &&
       componentRef.current &&
       !componentRef.current.contains(event.target)
-    ) {
-      close();
-    }
-  }
+    ) {close();}
+  },[close]);
 
   // eslint-disable-next-line consistent-return
   React.useEffect(() => {
@@ -43,24 +68,38 @@ export const ModalFlow = ({
         handleClickOutside(e);
       });
     }
-  }, [closeOnOutsideClick]);
-
-  React.useEffect(() => {
-    setActiveKeyLocal(activeKey);
-  }, [activeKey]);
+  }, [closeOnOutsideClick,handleClickOutside]);
 
   const handleNext = (index: unknown) => {
-    setActiveKeyLocal(index);
+    if(index){
+      handleActiveKeyChange(index);
+    }else{
+      let keys = Object.keys(components);
+      let activeIndex = keys.findIndex(e => e === activeKeyLocal);
+      if(activeIndex >= 0 && activeIndex !== keys.length - 1){
+        handleActiveKeyChange(keys[activeIndex + 1 ])
+      }
+    };
   };
 
   const handlePrev = (index: unknown) => {
-    setActiveKeyLocal(index);
+    if(index){
+      handleActiveKeyChange(index);
+    }else{
+      let keys = Object.keys(components);
+      let activeIndex = keys.findIndex(e => e === activeKeyLocal);
+      if(activeIndex > 0){
+        handleActiveKeyChange((keys[activeIndex - 1 ]));
+      }
+    };
   };
 
-  function close() {
-    setActiveKeyLocal(false);
-    if (onClose && typeof onClose === 'function') onClose();
+  const getBack = () => {
+    if(prevKey){
+      handleActiveKeyChange(prevKey);
+    }
   }
+
 
   const onChange = (newValues: any) => {
     setValues({ ...values, ...newValues });
@@ -73,7 +112,7 @@ export const ModalFlow = ({
     for (const i in components) {
       onlyComponents.push(components[i]);
       componentKeys.push(i);
-    }
+    };
 
     const childrenWithProps = React.Children.map(
       onlyComponents,
@@ -81,10 +120,11 @@ export const ModalFlow = ({
         if (React.isValidElement(child)) {
           return React.cloneElement(child, {
             values,
+            onChange,
             handlePrev,
             handleNext,
-            onChange,
-            closeModal: close
+            close,
+            getBack,
           });
         }
         return child;
@@ -95,24 +135,20 @@ export const ModalFlow = ({
       component: e,
       key: componentKeys[index]
     }));
-
+  
     setChildrens(childrensWithKey);
-  }, [components]);
+  
+  }, [components,activeKeyLocal,values]);
 
-  let activeIndex = null;
   let activeComponent = null;
 
-  if (childrens && activeKey) {
-    activeIndex = childrens.findIndex((e) => e.key === activeKeyLocal);
-    activeComponent =
-      childrens && childrens[activeIndex] && childrens[activeIndex].component
-        ? childrens[activeIndex].component
-        : null;
-  }
+  if (childrens && activeKeyLocal) {
+    activeComponent = childrens.find(e => e.key === activeKeyLocal).component;
+  };
 
   return (
-    <div className={`modal ${activeComponent && 'modal--visible'}`}>
-      <div ref={componentRef} className={`modal-body`}>
+    <div style={{...modalStyles}} className={`modal-flow ${activeComponent && 'modal-flow--visible'}`}>
+      <div style={{...bodyStyles}} ref={componentRef} className={`modal-flow-body`}>
         <div>{activeComponent && activeComponent}</div>
       </div>
     </div>
